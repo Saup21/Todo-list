e0fd9a8b2d8a7f68bbe92020cad53c0e771ef799
Saup21 <sauparna.gupta@gmail.com>
2021-05-10 03:04:17 +0530

    Changed static files and templates

---
+++ todo/static/js/todo.js
@@ -0,0 +1,30 @@
+//
+// $(document).ready(function() {
+//     $('.drop_down_Icon').click(function() {
+//         $('ul').toggleClass('actv');
+//         $('li.right').toggle(400);
+//     });
+//
+//     $('.right, .left, .drop_down_Icon').click(function(e) {
+//         console.log(e.target.value);
+//         e.preventDefault();
+//     });
+// });
+//
+// $(window).resize(function() {
+//     if($(window).width()>=785) {
+//         $('li.right').show();
+//     }
+//     else {
+//         $('li.right').hide();
+//     }
+// });
+//
+// $(window).scroll(function (e) {
+//     if(window.scrollY > 150)
+//     $('ul.nbar').slideDown();
+//     else {
+//         $('ul.nbar').slideUp();
+//     }
+//     console.log(window.scrollY);
+// });
--- todosite/settings.py
+++ todosite/settings.py
@@ -133,6 +133,9 @@ MESSAGE_TAGS = {
 STATIC_URL = '/static/'
 
 STATIC_ROOT =  BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
