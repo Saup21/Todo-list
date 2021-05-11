92024cd5b19454c9f10f3139e3fb7717eef86dd1
Saup21 <sauparna.gupta@gmail.com>
2021-05-11 23:20:16 +0530

    Changed front-end

--- todo/static/js/todo.js
+++ todo/static/js/todo.js
@@ -1,15 +1,11 @@
-//
-// $(document).ready(function() {
-//     $('.drop_down_Icon').click(function() {
-//         $('ul').toggleClass('actv');
-//         $('li.right').toggle(400);
-//     });
-//
-//     $('.right, .left, .drop_down_Icon').click(function(e) {
-//         console.log(e.target.value);
-//         e.preventDefault();
-//     });
-// });
+$(document).ready(function() {
+    $('.btn-light').click(function() {
+        console.log('1');
+        $('li.hide').toggleClass('actv');
+        $('li.hide').toggle(400);
+        console.log('2');
+    });
+});
 //
 // $(window).resize(function() {
 //     if($(window).width()>=785) {
